e4f11795df963ca5620353e64ab9c50ed6c49ba4
kouakou kouamé yannick <yannickkouakou13@gmail.com>
2024-07-27 19:18:40 +0200

    chore: Update App.vue and Header.vue components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,17 @@
-<script setup la
-  components: { Container },ng="ts">
+<script setup la components: { Container },ng="ts">
 import Header from './components/Header.vue';
 import Container from './layout/Container.vue';
 import Form from './components/Form.vue';
+import { Toaster } from '@steveyuowo/vue-hot-toast';
+import "@steveyuowo/vue-hot-toast/vue-hot-toast.css";
 </script>
 
 <template>
+  <Toaster />
   <container>
-   <Header />
-   <Form />
+    <Header />
+    <Form />
   </container>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -2,7 +2,9 @@
 
 <template>
     <h1>Linkmini</h1>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae dolores commodi nobis iure pariatur itaque.
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae dolores commodi nobis iure pariatur
+        itaque.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae dolores commodi nobis iure pariatur
+        itaque.
     </p>
 </template>
 
@@ -15,7 +17,7 @@ p {
     width: 80%;
     margin: 1rem auto;
     max-width: 600px;
-    font-size: 1.1rem;
+    font-size: 1rem;
     color: #334155;
     text-align: center;
 }
