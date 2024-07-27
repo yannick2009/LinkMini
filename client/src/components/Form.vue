5b340906faf1e0c66d472a031bfbe0244d0fc086
kouakou kouamé yannick <yannickkouakou13@gmail.com>
2024-07-27 19:15:51 +0200

    chores: work on the from UI

--- client/src/components/Form.vue
+++ client/src/components/Form.vue
@@ -1,14 +1,37 @@
-<script setup>
-function onSubmit(event) {
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import { urlRegex } from '../constant';
+import { toast } from '@steveyuowo/vue-hot-toast';
+
+
+const isValid = ref<boolean>(true);
+const longUrl = ref<string>('');
+function onSubmit(event: Event) {
     event.preventDefault();
+    toast.success('Form submitted');
 }
+
+watch(longUrl, (newVal) => {
+    if (longUrl.value.length > 0 && !urlRegex.test(newVal)) {
+        isValid.value = false;
+    } else {
+        isValid.value = true;
+    }
+});
+
 </script>
 
 <template>
+    <form>
+        <label>QR Code</label>
+        <div class="qrcode"></div>
+        <label for="short">short URL</label>
+        <input disabled id="short" type="text">
+    </form>
     <form @submit="onSubmit">
-        <label for="url">Lien URL</label>
-        <input id="url" type="text">
-        <button>Generer</button>
+        <label for="long">Your long URL</label>
+        <input :class="!isValid ? 'invalid' : ''" v-model="longUrl" id="long" type="text">
+        <button :disabled="!isValid || longUrl.trim().length == 0">Generate</button>
     </form>
 </template>
 
@@ -17,7 +40,7 @@ form {
     width: 50%;
     width: 310px;
     display: flex;
-    margin: 2rem auto;
+    margin: .5rem auto;
     flex-direction: column;
     align-items: center;
     justify-content: center;
@@ -25,6 +48,15 @@ form {
     background: white;
     border-radius: 6px;
     box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);
+}
+
+form .qrcode {
+    width: 100%;
+    height: 170px;
+    background: #F3F4F6;
+    border-radius: 5px;
+    margin-bottom: 1rem;
+    border: 1px dashed #2563EB;
 }
 
 form>* {
@@ -36,29 +68,38 @@ form>label {
     font-size: 1.2rem;
     font-weight: 700;
     color: #334155;
-    margin-bottom: .5rem;
+    margin: .5rem 0;
 }
 
 form>input {
-    padding: 1rem;
+    padding: 1.2rem;
     margin: .5rem 0;
     border: 1px solid #2563EB;
     border-radius: 5px;
     background: #DBEAFE;
 }
 
+form input:disabled {
+    background: #EFF6FF;
+    border: 1px dashed #2563EB;
+}
+
 form>button {
-    padding: 1rem;
+    padding: 1.2rem;
     background: #2563EB;
     color: white;
     border: none;
     margin: .5rem 0;
-    /* padding: .5rem; */
     cursor: pointer;
     border-radius: 5px;
 }
 
 form>button:hover {
     background: #1E40AF;
+}
+
+.invalid {
+    border: 1px solid #E11D48;
+    background: #FECDD3;
 }
 </style>
