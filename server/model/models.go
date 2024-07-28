56c091cb1b5e9bb8dcf4103640db864fd9d23f09
yannick2009 <yannickkouakou13@gmail.com>
2024-07-28 15:30:48 +0200

    chore: Update QrCode field in URL model to use byte array

--- server/model/models.go
+++ server/model/models.go
@@ -7,7 +7,7 @@ type URL struct {
 	LongURL   string    `json:"longUrl"`
 	URLHash   string    `json:"urlHash"`
 	ClickNum  int       `json:"clickNum"`
-	QrCode    string    `json:"qrCode"`
+	QrCode    []byte    `json:"qrCode"`
 	CreatedAt time.Time `json:"createdAt"`
 	ExpireAt  time.Time `json:"expireAt"`
 }
